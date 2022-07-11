bd4b7df5976bce64df4916d1b97e75749d7cacc0
nicasop <nicolas_villita21@hotmail.es>
2022-07-10 19:29:35 -0500

    prcocfile

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const puerto = 3000;
 app = express();
 
 app.use(express.static('public'))
 
 app.listen( puerto, () => {
-    console.log(`Servidor corriendo en el puerto ${ puerto }`);
+    console.log(`Servidor corriendo en el puerto ${ process.env.Port }`);
 });
