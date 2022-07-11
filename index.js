97a40400b9475253ac39455c1fcf9b2421c7de2c
nicasop <nicolas_villita21@hotmail.es>
2022-07-10 19:46:34 -0500

    ruta hola eliminada

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ app = express();
 
 app.use(express.static('public'))
 
-app.route('/hola', (req, res) => {
-    res.send(`<h1>PRUEBA</h1>`)
-})
-
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
 });
